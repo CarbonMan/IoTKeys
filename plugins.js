6c99f3fcd88bc5689f7e1c11599494c7144c7967
CarbonMan <CarbonMan@users.noreply.github.com>
2020-06-11 23:46:41 +1000

    Update plugins.js

--- plugins.js
+++ plugins.js
@@ -3,7 +3,7 @@
     {
       "category": "Device",
       "type": "Scale",
-      "name": "SF-886 scales",
+      "name": "SF-884 scales",
       "version": "0.1",
       "descriptionLocation": "devices/SF-884/description-0.1.txt",
       "driverLocation": "devices/SF-884/SF-884-0.1.js",
