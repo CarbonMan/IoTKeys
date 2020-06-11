7c3b11c1be70bd79e580d8c8af95456a16db5efc
CarbonMan <CarbonMan@users.noreply.github.com>
2020-06-11 16:22:09 +1000

    Create plugins.js

---
+++ plugins.js
@@ -0,0 +1,9 @@
+{
+  "SF-886 scales":{
+      "type": "Device",
+      "version": 0.1,
+      "description": "devices/SF-886/description-01.txt",
+      "driver": "devices/SF-886/SF-884-01.js",
+      "targetDirectory": "scripts"
+  }
+}
