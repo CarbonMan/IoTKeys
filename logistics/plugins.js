d3701e2a3306c5a357714dcb635b8c1c41b6f339
CarbonMan <CarbonMan@users.noreply.github.com>
2020-06-11 14:52:50 +1000

    Create plugins.js

---
+++ logistics/plugins.js
@@ -0,0 +1,10 @@
+{
+  "Australia Post":{
+    "description": {
+      "file": "logistics/Australia Post/description-0.1.txt"
+    },
+    "urlMap": {
+      "file": "logistics/Australia Post/urlMap-0.1.json"
+    }
+  }
+}
