fe617db5512bb82c5c08fd3f40d961308cf00b26
CarbonMan <CarbonMan@users.noreply.github.com>
2020-06-21 11:52:38 +1000

    Update ebayOrder-0.1.js

--- scripts/ebay/ebayOrder/ebayOrder-0.1.js
+++ scripts/ebay/ebayOrder/ebayOrder-0.1.js
@@ -1,4 +1,9 @@
 console.log("Ebay receiver loaded");
+var ebayOrdersCSV = host.getInputFileContents("data/ebayOrders.csv");
+var ebayOrders = [];
+if (ebayOrdersCSV){
+	ebayOrders = Papa.parse(ebayOrdersCSV, {skipEmptyLines:true});
+}
 this.ebayOrder = function(orderNumber){
   return new EbayOrder();
 };
