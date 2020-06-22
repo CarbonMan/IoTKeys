8c73766508f567e20380a56204eff78fb917e9fd
CarbonMan <CarbonMan@users.noreply.github.com>
2020-06-22 23:35:19 +1000

    Update ebayOrder-0.1.js

--- scripts/ebay/ebayOrder/ebayOrder-0.1.js
+++ scripts/ebay/ebayOrder/ebayOrder-0.1.js
@@ -11,25 +11,27 @@ if (ebayOrdersCSV) {
 }
 this.ebayOrder = function (orderNumber) {
     // 22-05196-87110
+    if (!orderNumber)
+        return null;
     var order = ebayOrders.data.find((order) => {
-        return order[1] == orderNumber;
-    });
-	if (order)
-		return new EbayOrder();
-	else
-		return null;
+            return order[1] == orderNumber;
+        });
+    if (order)
+        return new EbayOrder(order);
+    else
+        return null;
+    function EbayOrder(order) {
+        this.getReceiver = function () {
+            return {
+                name: order[12],
+                phone: order[13],
+                address1: order[15],
+                /* address2: order[15], */
+                city: order[16],
+                state: order[17],
+                postalCode: order[18],
+                country: order[19]
+            };
+        };
+    }
 };
-function EbayOrder(order) {
-	this.getReceiver = function(){
-		return {
-			name: order[12],
-			phone: order[13],
-			address1: order[14],
-			address2: order[15],
-			city: order[16],
-			state: order[17],
-			postalCode: order[18],
-			country: order[19]
-		};
-	};
-}
