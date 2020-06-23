e9b4c4783b6a1d8f36d62b2107664154709a6086
CarbonMan <CarbonMan@users.noreply.github.com>
2020-06-23 22:56:59 +1000

    Update ebayOrder-0.1.js

--- scripts/ebay/ebayOrder/ebayOrder-0.1.js
+++ scripts/ebay/ebayOrder/ebayOrder-0.1.js
@@ -1,4 +1,11 @@
 console.log("Ebay receiver loaded");
+var states=[],statesCSV = host.getInputFileContents("data/states.csv");
+if (statesCSV) {
+    setTimeout(() => {
+        // PapaParse has to load
+        states = Papa.parse(statesCSV);
+    }, 0);
+}
 var ebayOrdersCSV = host.getInputFileContents("data/ebayOrders.csv");
 var ebayOrders = [];
 if (ebayOrdersCSV) {
@@ -22,13 +29,17 @@ this.ebayOrder = function (orderNumber) {
         return null;
     function EbayOrder(order) {
         this.getReceiver = function () {
+			// Convert from a state name to an abbreviation
+			var state = states.data.find((s)=>{
+				return s[0] == order[17].toUpperCase();
+			});
             return {
                 name: order[12],
                 phone: order[13],
                 address1: order[15],
                 /* address2: order[15], */
                 city: order[16],
-                state: order[17],
+                state: state[1],
                 postalCode: order[18],
                 country: order[19]
             };
