46ffac3ad80f2c12aba442b924139d718cea365f
CarbonMan <CarbonMan@users.noreply.github.com>
2020-06-25 23:38:40 +1000

    Update csvInventory-0.1.js

--- scripts/csvInventory/csvInventory-0.1.js
+++ scripts/csvInventory/csvInventory-0.1.js
@@ -22,12 +22,64 @@ function csvInventory() {
         });
     this.removeFromInventory = function (options) {
         // order, sku, quantity
+        var locations = [];
+        inventory.forEach((i) => {
+            if (i.sku == options.sku && i.qty > 0) {
+                locations.push(i);
+            }
+        });
+        var remaining = options.qty;
+        locations.forEach((i) => {
+            if (i.qty > 0) {
+                var history = {
+                    date: formatDate(),
+                    order: options.order,
+                    sku: options.sku,
+                    location: i.location
+                };
+                if (i.qty - remaining >= 0) {
+                    i.qty -= remaining;
+                    history.qty = -remaining;
+                    remaining = 0;
+                } else {
+                    i.qty = 0;
+                    history.qty = -i.qty;
+                    remaining -= i.qty;
+                }
+                orderHistory.push(history);
+            }
+        });
+        if (!locations.length) {
+            inventory.push({
+                sku: options.sku,
+                qty: -options.qty
+            });
+            orderHistory.push({
+                date: formatDate(),
+                order: options.order,
+                sku: options.sku,
+                qty: options.qty
+            });
+        }
         this.save();
     };
     this.save = function () {
         var csv = Papa.unparse(inventory);
         host.strToFile(csv, "data/inventory.csv");
         var csv = Papa.unparse(orderHistory);
-        host.strToFile(csv, "data/inventory.csv");
+        host.strToFile(csv, "data/orderHistory.csv");
     };
+    function formatDate(date) {
+        var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+        if (month.length < 2)
+            month = '0' + month;
+        if (day.length < 2)
+            day = '0' + day;
+
+        return [year, month, day].join('-');
+    }
 }
