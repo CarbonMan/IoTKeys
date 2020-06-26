7353608f0be6213204d948512f93908dfbf64b9f
CarbonMan <CarbonMan@users.noreply.github.com>
2020-06-26 15:17:42 +1000

    Update ebayOrder-0.1.js

--- scripts/ebay/ebayOrder/ebayOrder-0.1.js
+++ scripts/ebay/ebayOrder/ebayOrder-0.1.js
@@ -1,5 +1,5 @@
 console.log("Ebay receiver loaded");
-var states=[],statesCSV = host.getInputFileContents("data/states.csv");
+var states = [], statesCSV = host.getInputFileContents("data/states.csv");
 if (statesCSV) {
     setTimeout(() => {
         // PapaParse has to load
@@ -20,31 +20,42 @@ this.ebayOrder = function (orderNumber) {
     // 22-05196-87110
     if (!orderNumber)
         return null;
-    var order = ebayOrders.data.find((order) => {
+    var orders = ebayOrders.data.filter(order => {
             return order[1] == orderNumber;
         });
-    if (order)
-        return new EbayOrder(order);
+    if (orders.length)
+        return new EbayOrder(orders);
     else
         return null;
-    function EbayOrder(order) {
-		// No sender is recorded in an ebay order
+    function EbayOrder(orders) {
+        // No sender is recorded in an ebay order
+        var details = orders[0];
+        this.items = [];
+        orders.forEach(item => {
+            if (item[20]) {
+                // ebay multiline orders have a summary line with no item detail
+                this.items.push({
+                    sku: item[20],
+                    description: item[21],
+                    qty: item[24]
+                });
+            }
+        });
         this.getReceiver = function () {
-			// Convert from a state name to an abbreviation
-			var state = states.data.find((s)=>{
-				return s[0] == order[17].toUpperCase();
-			});
+            // Convert from a state name to an abbreviation
+            var state = states.data.find((s) => {
+                    return s[0] == details[17].toUpperCase();
+                });
             return {
-                name: order[12],
-                phone: order[13],
-                address1: order[15],
+                name: details[12],
+                phone: details[13],
+                address1: details[15],
                 /* address2: order[15], */
-                city: order[16],
+                city: details[16],
                 state: state[1],
-                postalCode: order[18],
-                country: order[19]
+                postalCode: details[18],
+                country: details[19]
             };
         };
     }
 };
-	
