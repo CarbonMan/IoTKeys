7a1f1b02e8deda7dd8c674ae4a1f820495cae33b
CarbonMan <CarbonMan@users.noreply.github.com>
2020-06-28 18:32:11 +1000

    Update ebayOrder-0.1.js

--- scripts/ebay/ebayOrder/ebayOrder-0.1.js
+++ scripts/ebay/ebayOrder/ebayOrder-0.1.js
@@ -50,7 +50,7 @@ this.ebayOrder = function (options) {
                 /* address2: order[15], */
                 city: details[16],
                 stateCode: (state ? state.code: details[17]),
-                stateName: (state ? state.code: details[17]),
+                stateName: (state ? state.name: details[17]),
                 postalCode: details[18],
                 countryCode: (country ? country.code : details[19]),
                 countryName: (country ? country.name : details[19])
