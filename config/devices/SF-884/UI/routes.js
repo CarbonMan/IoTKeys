56b6b396e9cf1b7b0f4c52766d7a3bcda5bb8ff8
CarbonMan <CarbonMan@users.noreply.github.com>
2020-08-09 11:26:04 +1000

    Update routes.js

--- config/devices/SF-884/UI/routes.js
+++ config/devices/SF-884/UI/routes.js
@@ -1,4 +1,6 @@
-var config = require("../SF-884-INF-0.1.json");
+var fs = require("fs"),
+path = require("path"),
+config = require("../SF-884-INF-0.1.json");
 
 function routes(opts) {
     opts.app.get("/devices/SF-884/config", function (req, res, next) {
@@ -8,6 +10,16 @@ function routes(opts) {
             res.send(str).end();
         });
     });
+    opts.app.post("/devices/SF-884/config", function (req, res) {
+		console.log(req.body);
+		config = req.body;
+		fs.writeFile( path.join(__dirname, "..", "SF-884-INF-0.1.json"),
+			JSON.stringify(config),
+			()=>{
+				res.end();
+			}
+		);
+	});
 };
 
 module.exports = routes;
